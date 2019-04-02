8cca97f8ab284731273828fb89386e335d897d41
Luo Reid <luo.reid@gmail.com>
2019-04-02 23:14:49 +0800

    jest snapshot

--- src/components/BaseButton.spec.js
+++ src/components/BaseButton.spec.js
@@ -1,5 +1,6 @@
 import BaseButton from './BaseButton.vue'
 import { shallow } from 'vue-test-utils'
+import { createRenderer } from 'vue-server-renderer';
 
 describe('BaseButton', () => {
   // test
@@ -8,5 +9,22 @@ describe('BaseButton', () => {
     const wrapper = shallow(BaseButton)
     wrapper.trigger('click')
     expect(wrapper.emitted().click).toBeTruthy()
+  })
+  test('snapshot',()=>{
+    const render = createRenderer()
+    const wrapper = shallow(BaseButton,{
+      propsData:{
+        icon:'add',
+        disabled:true,
+        badge:'3',
+      },
+      slots:{
+        default:'<span> Add Item</span>',
+      },
+    })
+    render.renderToString(wrapper.vm,(err,str) => {
+      if(err) throw new Error(err)
+      expect(str).toMatchSnapshot()
+    })
   })
 })
