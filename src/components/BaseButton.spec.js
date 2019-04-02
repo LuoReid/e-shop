7c5098d641faad8a243bf669debaff37aae579ec
Luo Reid <luo.reid@gmail.com>
2019-04-02 10:09:09 +0800

    upgrade to babel7 step2

--- src/components/BaseButton.spec.js
+++ src/components/BaseButton.spec.js
@@ -2,11 +2,11 @@ import BaseButton from './BaseButton.vue'
 import { shallow } from 'vue-test-utils'
 
 describe('BaseButton', () => {
-    //test
-    test('click event', () => {
-        //test code
-        const wrapper = shallow(BaseButton)
-        wrapper.trigger('click')
-        expect(wrapper.emitted().click).toBeTruthy()
-    })
+  // test
+  test('click event', () => {
+    // test code
+    const wrapper = shallow(BaseButton)
+    wrapper.trigger('click')
+    expect(wrapper.emitted().click).toBeTruthy()
+  })
 })
